7252769fd3ad8831df6bf5e5446a0f31c07ced16
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2022-10-16 12:59:29 +0300

    initial com

---
+++ lib/Extended_chess.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "Extended_chess/version"
+
+module ExtendedChess
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "Extended_chess"
+
+require "minitest/autorun"
